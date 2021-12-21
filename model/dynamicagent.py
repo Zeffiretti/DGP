99d0e2b136f2d8395e63a6783828bec3732bf10a
zeffiretti <hiesh@mail.com>
2021-12-21 21:51:50 +0800

    tested on 20000 frames

--- model/dynamicagent.py
+++ model/dynamicagent.py
@@ -10,11 +10,15 @@ import gpytorch
 import numpy as np
 from .labeler import Label
 from .modgp import MultioutputDGPModel
+from .interpolation import QuinticPolynomial
 import multiprocessing
 from multiprocessing import Process
 import os
 from scipy.io import savemat
 import warnings
+import timeit
+import scipy
+import matplotlib.pyplot as plt
 
 
 class DynamicAgent:
@@ -22,15 +26,17 @@ class DynamicAgent:
   This class is to combine the results from 4 different Labelers.
   """
 
-  def __init__(self, all_data: type(torch.tensor([])), gaussian_window=120, save_mat=None):
+  def __init__(self, all_data: type(torch.tensor([])), gaussian_window=120, save_mat=None, normalize=False):
     # os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+    self.retrain = False
     self.tail_index = 0
     self.gaussian_window = gaussian_window
     self.header_index = self.tail_index + self.gaussian_window
     self.checks = []
     self.save_mat = save_mat
-    self.loose_fit = 1
+    self.loose_fit = 2
     self.exp = 1.0 + 1e-3
+    self.iter = 1
     # self.all_data = torch.nan_to_num(all_data, nan=1000)
     self.all_data = all_data
     self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -39,29 +45,44 @@ class DynamicAgent:
       raise TypeError("input data variables not of type torch.tensor")
     print('input date shape:', self.all_data.shape)
     self.all_data.to(self.device)
-    print('before notmalize: \n', self.all_data[:10, :4])
-    self.all_data = torch.nan_to_num(self.all_data, nan=0)
-    self.all_data[:, 1:] = torch.nn.functional.normalize(self.all_data[:, 1:], dim=0) * 300
-    print('after notmalize: \n', self.all_data[:10, :4])
-    # todo: 4 labelers are to be created
+    # print('before notmalize: \n', self.all_data[:10, :4])
+    # self.all_data = torch.nan_to_num(self.all_data, nan=0)
+    self.all_data[:, 1:] = torch.nn.functional.normalize(self.all_data[:, 1:], dim=0) * 300 if normalize \
+      else self.all_data[:, 1:] * 30
+    self.all_data[:, 0] = self.all_data[:, 0] * 30
+    # print('after normalize: \n', self.all_data[:10, :4])
+    # finished: 4 labelers are to be created
     self.g_number = [0, 4 * 3, 7 * 3, 11 * 3, 14 * 3]
     self.split_data(self.all_data[self.tail_index:self.header_index, 1:])  # rearrange all data into four groups
     labels_master = []
     for g in self.groups:
       labels_master.append(Label(self.all_data[self.tail_index:self.header_index, 0], g, self.device))
     self.masters = labels_master
+
+    self.lost_start_idx = torch.ones((14, 1)) * -1
+    self.lost_end_idx = torch.ones((14, 1)) * -1
+    start_time = timeit.default_timer()
     self.run()  # runrunrun
+    end_time = timeit.default_timer()
+    print("consume time:", (end_time - start_time) / 60, 'min')
 
   def run(self):
     # todo: running process
     self.train_model()
     self.predict()
     print('round 1 finished!')
-    while self.tail_index < 15000:
+    # self.fig, self.ax = plt.subplots()
+    while self.tail_index < 20000:
       self.update_data()
-      self.train_model(max_iter=5)
+      if self.retrain:
+        self.train_model(max_iter=100)
+        self.retrain = False
+      else:
+        self.train_model(max_iter=self.iter)
+        self.iter = 1
       self.predict()
-      warnings.warn('round {0} finished at {1}!'.format(self.tail_index, self.test_time))
+      print('\033[44m\033[1;30mround \033[1;31m {0:6d} \033[1;30m finished at \033[1;31m {1:3.6f} \033[1;30ms! \033[0m' \
+            .format(self.tail_index, self.test_time.item() / 30))
     # print('checks is ', self.checks)
     res_mean_mat = None
     res_upper_mat = None
@@ -142,10 +163,13 @@ class DynamicAgent:
     self.tail_index += 1
     self.header_index = self.tail_index + self.gaussian_window
     update_time = self.all_data[self.tail_index:self.header_index, 0]
-    print('train data is', self.all_data[self.header_index - 1, 10:13])
+    # print('train data is', self.all_data[self.header_index - 1, 10:13])
     self.split_data(self.all_data[self.tail_index:self.header_index, 1:])  # rearrange all data into four groups
     for master, g in zip(self.masters, self.groups):
       master.set_train_data(update_time, g)
+    # plt.clf()
+    # plt.plot(update_time.cpu() / 30, self.all_data[self.tail_index:self.header_index, 1:].cpu())
+    # plt.show()
 
   def fit(self):
     """
@@ -154,93 +178,181 @@ class DynamicAgent:
     """
     # the not-permuted matrix are to be handled
     if not torch.all((self.permutation.matmul(self.permutation.T)).eq(torch.eye(14).to(self.device))):
-      print('before pos,', self.test_pos)
-      handled_data = self.test_pos.detach().view(14, 3)
-      predication_data = self.pred.detach().view(14, 3)
-      col_sum, row_sum = torch.sum(self.permutation, 0), torch.sum(self.permutation, 1)
-      # step 1: process all conflict labels
-      # howto: evaluate distance from the conflict points to each label, and pair the nearest
-      conflict_labels = torch.where(row_sum > 1)[0]  # 0th of the tuple contains one tensor
-      print('before step 1,\n', self.permutation)
-      for conflict_label_idx in conflict_labels:
-        print('labels conflict at ', conflict_label_idx)
-        print('conflict details:\n', self.permutation[conflict_label_idx, :])
-        nebulous_idxes = torch.where(self.permutation[conflict_label_idx, :].detach() == 1)[0]
-        # print("nebulous_idx is \n", nebulous_idxes)
-        row = conflict_label_idx
-        distance = 1000
-        col = 0
-        for nebulous_idx in nebulous_idxes:
-          self.permutation[row, nebulous_idx] = 0
-          warnings.warn('nebulous labels!!!')
-          # print('predication is', predication_data[row, :])  # the latest predications
-          # print('handled data is', handled_data[nebulous_idx, :])
-          cdist = torch.cdist(handled_data[nebulous_idx, :].view(1, -1), predication_data[row, :].view(1, -1))
-          if cdist < distance:
-            col = nebulous_idx
-            distance = cdist
-          # print('distance is', distance)
-          point = handled_data[row, :]
-        self.permutation[row, col] = 1.0
+      self.interpolation_idxes = []
+      print('before fitting,', self.test_pos)
+      self.handled_data = self.test_pos.detach().view(14, 3)
+      self.predication_data = self.pred.detach().view(14, 3)
+      self.last_data = self.all_data[self.header_index - 1, 1:].detach().view(14, 3)
+      assert not torch.isnan(torch.sum(self.predication_data)), 'predication contains nan number at {0}'.format(self.header_index)
+      self.fit_conflict_labels()
       print('after step 1,\n', self.permutation)
-
-      col_sum, row_sum = torch.sum(self.permutation, 0), torch.sum(self.permutation, 1)
-      conflict_points = torch.where(col_sum > 1)[0]  # 0th of the tuple contains one tensor
-      for conflict_point_idx in conflict_points:
-        print('points conflict at ', conflict_point_idx)
-        print('conflict details:\n', self.permutation[:, conflict_point_idx])
-        nebulous_idxes = torch.where(self.permutation[:, conflict_point_idx].detach() == 1)[0]
-        # print("nebulous_idx is \n", nebulous_idxes)
-        col = conflict_point_idx
-        distance = 1000
-        row = 0
-        for nebulous_idx in nebulous_idxes:
-          self.permutation[nebulous_idx, col] = 0
-          warnings.warn('nebulous points!!!')
-          # print('predication is', predication_data[col, :])  # the latest predications
-          # print('handled data is', handled_data[nebulous_idx, :])
-          cdist = torch.cdist(handled_data[nebulous_idx, :].view(1, -1), predication_data[col, :].view(1, -1))
-          if cdist < distance:
-            row = nebulous_idx
-            distance = cdist
-          # print('distance is', distance)
-          # point = handled_data[row, :]
-        self.permutation[row, col] = 1.0
+      self.fit_conflict_points()
       print('after step 2,\n', self.permutation)
-
-      col_sum, row_sum = torch.sum(self.permutation, 0), torch.sum(self.permutation, 1)
-      conflict_labels = torch.where(row_sum > 1)[0]  # 0th of the tuple contains one tensor
-      assert conflict_labels.shape[0] == 0, "conflict still exists after step 1!"
-      lost_points = torch.where(col_sum == 0)[0]  # 🙂
-      lost_labels = torch.where(row_sum == 0)[0]  # 0th of the tuple contains one tensor
-      if not lost_labels.shape[0] == lost_points.shape[0]:
-        print(self.permutation)
-      assert lost_labels.shape[0] == lost_points.shape[0], 'number of lost labels and points are not equal, ' \
-                                                           'please handle it.'
-      for idx, lost_label_idx in enumerate(lost_labels):
-        # print('lost label index is', lost_label_idx, idx)
-        row = lost_label_idx
-        col = lost_points[idx]
-        self.permutation[row, col] = 1
-        print('col={0},row={1}'.format(row, col))
-        print('predication is', predication_data[row, :])
-        print('and data is ', handled_data[row, :])
-        print('nan check is', torch.isnan(handled_data[row, :]))
-        distance = torch.cdist(handled_data[row, :].view(1, -1), predication_data[row, :].view(1, -1))
-        if torch.sum(torch.isnan(handled_data[row, :]) == True) == 3 \
-          or distance > self.loose_fit:
-          # if the actual observed data is NaN or too large on cdist, replace it with predications
-          # print('good, fit it!😋')
-          handled_data[row, :] = predication_data[row, :]
-          self.loose_fit *= self.exp
-          warnings.warn("replace raw data with predication at {0}".format(self.header_index))
-        else:
-          self.loose_fit = 1
-          warnings.warn("reset loose fit to 1 at {0}".format(self.header_index))
-        print('distance is', distance, 'and loose fit is', self.loose_fit)
+      # todo: before fit lost points, recovery points are to be checked
+      for i, lost_start in enumerate(self.lost_start_idx):
+        if lost_start >= 0 and torch.sum(self.permutation[i, :]) == 1:
+          self.lost_end_idx[i] = self.header_index
+          self.interpolation_idxes.append(i)
+      self.fit_lost()
       # no need to explicitly change the items
       # print('before,', self.test_pos)
-      # self.test_pos = handled_data.view(1, -1)
-      print('after,', self.test_pos)
-      self.all_data[self.header_index, 1:] = self.test_pos.detach()
-      print('after set,', self.all_data[self.header_index, 1:])
+      # self.test_pos = self.handled_data.view(1, -1)
+      # print('after,', self.test_pos)
+      self.all_data[self.header_index, 1:] = self.handled_data.detach().view(-1, 1)
+      # print('after set,', self.all_data[self.header_index, 1:])
+      for idx in self.interpolation_idxes:
+        self.backward_interpolation(idx)
+    else:
+      self.iter = 1
+
+  def fit_conflict_labels(self):
+    """
+
+    :return:
+    """
+    col_sum, row_sum = torch.sum(self.permutation, 0), torch.sum(self.permutation, 1)
+    # step 1: fix all conflict labels
+    # howto: evaluate distance from the conflict points to each label, and pair the nearest
+    conflict_labels = torch.where(row_sum > 1)[0]  # 0th of the tuple contains one tensor
+    print('before step 1,\n', self.permutation)
+    for conflict_label_idx in conflict_labels:
+      print('labels conflict at ', conflict_label_idx)
+      print('conflict details:\n', self.permutation[conflict_label_idx, :])
+      nebulous_idxes = torch.where(self.permutation[conflict_label_idx, :].detach() == 1)[0]
+      # print("nebulous_idx is \n", nebulous_idxes)
+      row = conflict_label_idx
+      distance = 1000
+      col = 0
+      for nebulous_idx in nebulous_idxes:
+        self.permutation[row, nebulous_idx] = 0
+        warnings.warn('nebulous labels!!!')
+        # print('predication is', predication_data[row, :])  # the latest predications
+        # print('handled data is', self.handled_data[nebulous_idx, :])
+        cdist = torch.cdist(self.handled_data[nebulous_idx, :].view(1, -1), self.last_data[row, :].view(1, -1))
+        if cdist < distance:
+          col = nebulous_idx
+          distance = cdist
+        # print('distance is', distance)
+        # point = self.handled_data[row, :]
+      self.permutation[row, col] = 1.0
+
+  def fit_conflict_points(self):
+    """
+
+    :return:
+    """
+    col_sum, row_sum = torch.sum(self.permutation, 0), torch.sum(self.permutation, 1)
+    conflict_points = torch.where(col_sum > 1)[0]  # 0th of the tuple contains one tensor
+    for conflict_point_idx in conflict_points:
+      print('points conflict at ', conflict_point_idx)
+      print('conflict details:\n', self.permutation[:, conflict_point_idx])
+      nebulous_idxes = torch.where(self.permutation[:, conflict_point_idx].detach() == 1)[0]
+      # print("nebulous_idx is \n", nebulous_idxes)
+      col = conflict_point_idx
+      distance = 1000
+      row = 0
+      for nebulous_idx in nebulous_idxes:
+        self.permutation[nebulous_idx, col] = 0
+        warnings.warn('nebulous points!!!')
+        # print('predication is', predication_data[col, :])  # the latest predications
+        # print('handled data is', self.handled_data[nebulous_idx, :])
+        cdist = torch.cdist(self.handled_data[nebulous_idx, :].view(1, -1), self.last_data[col, :].view(1, -1))
+        if cdist < distance:
+          row = nebulous_idx
+          distance = cdist
+        # print('distance is', distance)
+        # point = self.handled_data[row, :]
+      self.permutation[row, col] = 1.0
+
+  def fit_lost(self):
+    """
+
+    :return:
+    """
+    col_sum, row_sum = torch.sum(self.permutation, 0), torch.sum(self.permutation, 1)
+    conflict_labels = torch.where(row_sum > 1)[0]  # 0th of the tuple contains one tensor
+    assert conflict_labels.shape[0] == 0, "conflict still exists after step 1!"
+    lost_points = torch.where(col_sum == 0)[0]  # 🙂
+    lost_labels = torch.where(row_sum == 0)[0]  # 0th of the tuple contains one tensor
+    if not lost_labels.shape[0] == lost_points.shape[0]:
+      print(self.permutation)
+    assert lost_labels.shape[0] == lost_points.shape[0], 'number of lost labels and points are not equal, ' \
+                                                         'please handle it.'
+    for idx, lost_label_idx in enumerate(lost_labels):
+      # print('lost label index is', lost_label_idx, idx)
+      row = lost_label_idx
+      col = lost_points[idx]
+      self.permutation[row, col] = 1
+      print('col={0},row={1}'.format(row, col))
+      print('last data is', self.last_data[row, :])
+      print('predication is', self.predication_data[row, :])
+      print('and data is ', self.handled_data[row, :])
+      print('nan check is', torch.isnan(self.handled_data[row, :]))
+      distance = torch.cdist(self.handled_data[row, :].view(1, -1), self.last_data[row, :].view(1, -1))
+      # todo: replace fit param dilatometric with posture check
+      if torch.sum(torch.isnan(self.handled_data[row, :]) == True) == 3 \
+        or distance > self.loose_fit:
+        # if the actual observed data is NaN or too large on cdist, replace it with predications
+        # print('good, fit it!😋')
+        self.handled_data[row, :] = self.predication_data[row, :]
+        self.loose_fit *= self.exp
+        # warnings.warn("replace raw data with predication at {0}".format(self.header_index))
+        if self.lost_start_idx[row] == -1:
+          self.lost_start_idx[row] = self.header_index
+      else:
+        # catch the lost value
+        if self.lost_start_idx[row] > 0:
+          self.lost_end_idx[row] = self.header_index
+          self.interpolation_idxes.append(row)
+        self.loose_fit = 2
+        # warnings.warn("reset loose fit to 1 at {0}".format(self.header_index))
+      print('\033[43mDistance is', distance, 'and loose fit is', self.loose_fit, '\033[0m')
+      pred_distance = torch.cdist(self.handled_data[row, :].view(1, -1), self.predication_data[row, :].view(1, -1))
+      print('\033[43mPred_distance is', pred_distance, '\033[0m')
+      self.iter = 1 if self.iter == 1 and (torch.isnan(pred_distance) or pred_distance < 0.8) else 30
+
+  def backward_interpolation(self, index):
+    """
+    to interpolate the lost data backwardly, then how to ....
+    :return:
+    """
+    # assertion
+    assert self.lost_start_idx[index] >= 0, "lost_start_idx is negative"
+    assert self.lost_end_idx[index] >= 0, "lost_end_idx is negative"
+    assert self.lost_end_idx[index] > self.lost_start_idx[index], "lost end index is not greater than lost start index"
+    # rethink: is it necessary to consider the differential range? now we prefer not
+    print('data at {0} should be interpolated.'.format(index))
+    print('start at {0}, end at {1}'.format(self.lost_start_idx[index], self.lost_end_idx[index]))
+    # print('all start is ', self.lost_start_idx)
+    # print('all end is ', self.lost_end_idx)
+    # todo next: consider how many points are used to inference the lost point
+    # todo next: decide in which method to interpolate the series
+    # todo next: overwrite the lost data
+    # todo next: check geometry satisfaction
+    s_idx, e_idx = self.lost_start_idx[index].int().item(), self.lost_end_idx[index].int().item()
+    # print('before lost data:', self.all_data[s_idx - 5:e_idx + 2, :])
+    lost = self.all_data[s_idx - 5:e_idx + 2, :].cpu().numpy()
+    lost_data = {'lost': lost}
+    savemat(self.save_mat, lost_data)
+    point_index_x = index * 3 + 1
+
+    start_x, start_y, start_z = self.all_data[s_idx, point_index_x:point_index_x + 3]
+    final_x, final_y, final_z = self.all_data[s_idx, point_index_x:point_index_x + 3]
+    start_time, final_time = self.all_data[[s_idx, e_idx], 0]
+    quint_x = QuinticPolynomial(start_time, start_x, 0, 0, final_time, final_x, 0, 0, device=self.device)
+    quint_y = QuinticPolynomial(start_time, start_y, 0, 0, final_time, final_y, 0, 0, device=self.device)
+    quint_z = QuinticPolynomial(start_time, start_z, 0, 0, final_time, final_z, 0, 0, device=self.device)
+
+    for idx in range(s_idx, e_idx):
+      t = self.all_data[idx, 0]
+      self.all_data[idx, point_index_x] = torch.tensor([quint_x.get_position(t)]).to(self.device)
+      self.all_data[idx, point_index_x + 1] = torch.tensor([quint_y.get_position(t)]).to(self.device)
+      self.all_data[idx, point_index_x + 2] = torch.tensor([quint_z.get_position(t)]).to(self.device)
+    print('backward interpolation finished!')
+    self.retrain = True
+
+    # after interpolation
+    self.lost_start_idx[index] = -1
+    self.lost_end_idx[index] = -1
+
+    # raise Exception('aha🤪 stop!')
--- model/interpolation.py
+++ model/interpolation.py
@@ -1,12 +1,14 @@
 import numpy as np
+import torch
 
 
 class QuinticPolynomial:
-  def __init__(self, initial_time, initial_pos, initial_vel, initial_acc, final_time, final_pos, final_vel, final_acc):
-    self.position_coeff_ = np.zeros((6, 1), dtype=np.float)
-    self.velocity_coeff_ = np.zeros((6, 1), dtype=np.float)
-    self.acceleration_coeff_ = np.zeros((6, 1), dtype=np.float)
-    self.time_varibles = np.zeros((1, 6), dtype=np.float)
+  def __init__(self, initial_time, initial_pos, initial_vel, initial_acc, final_time, final_pos, final_vel, final_acc, device=torch.device("cpu")):
+    self.device=device
+    self.position_coeff_ = torch.zeros((6, 1), dtype=torch.float).to(device)
+    self.velocity_coeff_ = torch.zeros((6, 1), dtype=torch.float).to(device)
+    self.acceleration_coeff_ = torch.zeros((6, 1), dtype=torch.float).to(device)
+    self.time_varibles = torch.zeros((1, 6), dtype=torch.float).to(device)
 
     if final_time > initial_time:
       self.initial_time = initial_time
@@ -24,31 +26,32 @@ class QuinticPolynomial:
       self.final_vel = final_vel
       self.final_acc = final_acc
 
-      self.time_mat = np.array(
+      self.time_mat = torch.tensor(
         [[self.initial_time ** 5, self.initial_time ** 4, self.initial_time ** 3, self.initial_time ** 2, self.initial_time, 1.0],
          [5.0 * self.initial_time ** 4, 4.0 * self.initial_time ** 3, 3.0 * self.initial_time ** 2, 2.0 * self.initial_time, 1.0, 0.0],
          [20.0 * self.initial_time ** 3, 12.0 * self.initial_time ** 2, 6.0 * self.initial_time, 2.0, 0.0, 0.0],
          [self.final_time ** 5, self.final_time ** 4, self.final_time ** 3, self.final_time ** 2, self.final_time, 1.0],
          [5.0 * self.final_time ** 4, 4.0 * self.final_time ** 3, 3.0 * self.final_time ** 2, 2.0 * self.final_time, 1.0, 0.0],
          [20.0 * self.final_time ** 3, 12.0 * self.final_time ** 2, 6.0 * self.final_time, 2.0, 0.0, 0.0]
-         ], dtype=np.float)
-      self.conditions_mat = np.array([self.initial_pos, self.initial_vel, self.initial_acc, self.final_pos, self.final_vel, self.final_acc], dtype=np.float)
+         ], dtype=torch.float).to(device)
+      self.conditions_mat = torch.tensor([self.initial_pos, self.initial_vel, self.initial_acc, self.final_pos, self.final_vel, self.final_acc],
+                                         dtype=torch.float).to(device)
       self.conditions_mat = self.conditions_mat.reshape((6, 1))
-      self.position_coeff_ = np.linalg.pinv(self.time_mat) @ self.conditions_mat
-      self.velocity_coeff_ = np.array([[0.0],
-                                       [5.0 * self.position_coeff_[0, 0]],
-                                       [4.0 * self.position_coeff_[1, 0]],
-                                       [3.0 * self.position_coeff_[2, 0]],
-                                       [2.0 * self.position_coeff_[3, 0]],
-                                       [1.0 * self.position_coeff_[4, 0]],
-                                       ])
-      self.acceleration_coeff_ = np.array([[0.0],
-                                           [0.0],
-                                           [20.0 * self.position_coeff_[0, 0]],
-                                           [12.0 * self.position_coeff_[1, 0]],
-                                           [6.0 * self.position_coeff_[2, 0]],
+      self.position_coeff_ = torch.linalg.pinv(self.time_mat) @ self.conditions_mat
+      self.velocity_coeff_ = torch.tensor([[0.0],
+                                           [5.0 * self.position_coeff_[0, 0]],
+                                           [4.0 * self.position_coeff_[1, 0]],
+                                           [3.0 * self.position_coeff_[2, 0]],
                                            [2.0 * self.position_coeff_[3, 0]],
-                                           ])
+                                           [1.0 * self.position_coeff_[4, 0]],
+                                           ]).to(device)
+      self.acceleration_coeff_ = torch.tensor([[0.0],
+                                               [0.0],
+                                               [20.0 * self.position_coeff_[0, 0]],
+                                               [12.0 * self.position_coeff_[1, 0]],
+                                               [6.0 * self.position_coeff_[2, 0]],
+                                               [2.0 * self.position_coeff_[3, 0]],
+                                               ]).to(device)
 
   def get_position(self, t):
     if t >= self.final_time:
@@ -65,7 +68,7 @@ class QuinticPolynomial:
       return self.current_pos
     else:
       self.current_time = t
-      self.time_varibles = np.array([t ** 5, t ** 4, t ** 3, t ** 2, t, 1.0], dtype=np.float)
+      self.time_varibles = torch.tensor([t ** 5, t ** 4, t ** 3, t ** 2, t, 1.0], dtype=torch.float).to(self.device)
       self.current_pos = (self.time_varibles @ self.position_coeff_)[0]
       self.current_vel = (self.time_varibles @ self.velocity_coeff_)[0]
       self.current_acc = (self.time_varibles @ self.acceleration_coeff_)[0]
